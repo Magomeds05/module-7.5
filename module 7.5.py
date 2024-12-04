c1dfccab04c0ca353a86a6c46a35c88cc7800c00
Magomeds05 <yusypovm@gmail.com>
2024-12-04 19:51:44 +0300

    Update module 7.5.py

--- module 7.5.py
+++ module 7.5.py
@@ -1,15 +1,15 @@
-import os
+import os, time
 
-print('Путь:', os.getcwd())
-if os.path.exists('first'):
-    os.chdir('first')
-else:
-    os.mkdir('first')
-    os.chdir('first')
-for i in os.walk('.'):
-    print(i)
+directory = '.'
 
-print(os.path.join)
+for root, dirs, files in os.walk(directory):
+  for file in files:
+    filepath = os.path.join(root, file)
+    filetime = os.path.getatime(filepath)
+    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+    filesize = os.path.getsize(filepath)
+    parent_dir = os.path.dirname(filepath)
+    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
 
 #Используйте os.walk для обхода каталога, путь к которому указывает переменная directory
 #Примените os.path.join для формирования полного пути к файлам.
@@ -18,16 +18,3 @@ print(os.path.join)
 #Используйте os.path.dirname для получения родительской директории файла.
 #Комментарии к заданию:
 #Ключевая идея – использование вложенного for
-
-#for root, dirs, files in os.walk(directory):
-  #for file in files:
-    #filepath = ?
-    #filetime = ?
-    #formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-    #filesize = ?
-    #parent_dir = ?
-    #print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
-
-#Так как в разных операционных системах разная схема расположения папок, тестировать проще всего в папке проекта (directory = “.”)
-#Пример возможного вывода:
-#Обнаружен файл: main.py, Путь: ./main.py, Размер: 111 байт, Время изменения: 11.11.1111 11:11, Родительская директория.
